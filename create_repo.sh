2537f3aca9616f746ee0ba6bf65d9c34a5e6c16d
Nikhil Mehalawat <nikhilmehalawat@gmail.com>
2025-12-11 21:06:36 +0530

    Delete create_repo.sh

--- create_repo.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-echo "This script is a helper. It won't download Colab notebooks. Please download notebooks manually from Colab and place them into the folders."
